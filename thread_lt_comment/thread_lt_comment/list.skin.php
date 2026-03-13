ca1ebd2df0ed30b649c8e0f6a1da3f257fbf3da1
8oon9oon-boop <8oon9oon@gmail.com>
2026-03-13 23:39:42 +0900

    Add files via upload

--- thread_lt_comment/thread_lt_comment/list.skin.php
+++ thread_lt_comment/thread_lt_comment/list.skin.php
@@ -5,49 +5,43 @@ include_once ($board_skin_path.'/color.config.php');
 add_stylesheet('<link rel="stylesheet" href="'.$board_skin_url.'/style.css">', 1);
 add_stylesheet('<link rel="stylesheet" href="'.$board_skin_url.'/txtggu/textggu.css">', 1);
 
-// 파일업로드 설정
 if ($board['bo_upload_count'] < 4) {
     sql_query("UPDATE {$g5['board_table']} SET bo_upload_count = '4' WHERE bo_table = '$bo_table'");
 }
 if ($board['bo_use_list_file'] != '1') {
     sql_query("UPDATE {$g5['board_table']} SET bo_use_list_file = '1' WHERE bo_table = '$bo_table'");
 }
-
 ?>
 <div <?if($board['bo_table_width']>0){?>style="max-width:<?=$board['bo_table_width']?><?=$board['bo_table_width']>100 ? "px":"%"?>;margin:0 auto;"<?}?>>
-<hr class="padding">
+
 <? if($board['bo_content_head']) { ?>
-	<div class="board-notice">
-		<?=stripslashes($board['bo_content_head']);?>
-	</div><hr class="padding" />
+    <div class="board-notice">
+        <?=stripslashes($board['bo_content_head']);?>
+    </div>
 <? } ?>
 
 <div class="board-skin-basic">
-	<!-- 게시판 카테고리 시작 { -->
-	<?php if ($is_category) { ?>
-	<nav id="navi_category">
-		<ul>
-			<?php echo $category_option ?>
-		</ul>
-	</nav>
-	<?php } ?>
-    <!-- } 게시판 카테고리 끝 -->
+    <?php if ($is_category) { ?>
+    <nav id="navi_category">
+        <ul>
+            <?php echo $category_option ?>
+        </ul>
+    </nav>
+    <?php } ?>
 
-<? if ($write_href) { ?>
+    <? if ($write_href) { ?>
     <div class="thread_btn">
-        <button class="pixel-txt" onclick="$('#thread_write').slideToggle('fast');">새 글 작성</button>
+        <button type="button" class="ui-btn pixel-txt" onclick="$('#thread_write').slideToggle('fast');">새 글 작성</button>
     </div>    
     <div class="thread_write" id="thread_write" style="display:none;">
         <div class="thread-write-box">
             <div class="write-header pixel-txt">새 글 작성</div>
-            <div class="write-body">
-                <? include($board_skin_path.'/write.skin.php'); ?>
-            </div>
+            <? include($board_skin_path.'/write.skin.php'); ?>
         </div>
     </div>
-<? } ?>
+    <? } ?>
 
-<? if ($_list_style == 'list') { ?>
+    <? if ($_list_style == 'list') { ?>
     <form name="fboardlist" id="fboardlist" action="./board_list_update.php" onsubmit="return fboardlist_submit(this);" method="post">
     <input type="hidden" name="bo_table" value="<?php echo $bo_table ?>">
     <input type="hidden" name="sfl" value="<?php echo $sfl ?>">
@@ -58,103 +52,96 @@ if ($board['bo_use_list_file'] != '1') {
     <input type="hidden" name="sod" value="<?php echo $sod ?>">
     <input type="hidden" name="page" value="<?php echo $page ?>">
     <input type="hidden" name="sw" value="">
-<? } ?>
+    <? } ?>
     
-	<div class="thread_wrap type_<?=$_list_style; ?>">
-		<? for ($i=0; $i<count($list); $i++) { ?>
-
-    <? if ($_list_style == 'thread') { // 타래형 ?>
-		<div class="thread-box theme-box<? if ($list[$i]['is_notice']) echo " bo_notice"; ?>">
-            <? include ($board_skin_path.'/list.skin.thread.php'); ?>
-            <!-- 타래 코멘트 리스트 -->
-            <?
-                $thread_co_cnt = sql_fetch("SELECT count(wr_id) AS cnt FROM {$write_table} WHERE wr_parent = '{$list[$i]['wr_id']}' AND wr_is_comment = '1' AND wr_reply =''");
-                $thread_co_sql = "SELECT * FROM (SELECT * FROM {$write_table} WHERE wr_parent = '{$list[$i]['wr_id']}' AND wr_is_comment = '1' AND wr_reply ='' ORDER BY wr_comment DESC limit 2) AS sub ORDER BY wr_comment ASC";
-                $thread_result = sql_query($thread_co_sql);
-            ?>
-            <? if ($thread_co_cnt['cnt'] > 0) { ?>
-            <div class="thread_co_wrap">
-                <? if ($thread_co_cnt['cnt'] > 2) { ?>
-                    <a href="<?=$list[$i]['href']?>">전체 보기</a>
-                <? } ?>
-                <? for ($k=0; $row=sql_fetch_array($thread_result); $k++) { ?>
-                    <div class="thread_co">
+    <div class="thread_wrap type_<?=$_list_style; ?>">
+        <? for ($i=0; $i<count($list); $i++) { ?>
+            <? if ($_list_style == 'thread') { ?>
+                <div class="thread-box theme-box<? if ($list[$i]['is_notice']) echo " bo_notice"; ?>">
                     <? include ($board_skin_path.'/list.skin.thread.php'); ?>
+                    
+                    <?
+                        $thread_co_cnt = sql_fetch("SELECT count(wr_id) AS cnt FROM {$write_table} WHERE wr_parent = '{$list[$i]['wr_id']}' AND wr_is_comment = '1' AND wr_reply =''");
+                        $thread_co_sql = "SELECT * FROM (SELECT * FROM {$write_table} WHERE wr_parent = '{$list[$i]['wr_id']}' AND wr_is_comment = '1' AND wr_reply ='' ORDER BY wr_comment DESC limit 2) AS sub ORDER BY wr_comment ASC";
+                        $thread_result = sql_query($thread_co_sql);
+                    ?>
+                    <? if ($thread_co_cnt['cnt'] > 0) { ?>
+                    <div class="thread_co_wrap">
+                        <? if ($thread_co_cnt['cnt'] > 2) { ?>
+                            <a href="<?=$list[$i]['href']?>" class="pixel-txt" style="color:var(--line); margin-bottom:10px; display:inline-block;">전체 보기</a>
+                        <? } ?>
+                        <? for ($k=0; $row=sql_fetch_array($thread_result); $k++) { ?>
+                            <div class="thread_co">
+                            <? include ($board_skin_path.'/list.skin.thread.php'); ?>
+                            </div>
+                        <? } ?>
                     </div>
-                <? } ?>
-            </div>
-            <? } ?>
-            <? if ($write_href) { ?>
-            <div class="thread_bottom">
-                <button type="button" class="txt-point" onclick="writeThreadComment(<?=$list[$i]['wr_parent'];?>,'<?=$list[$i]['ca_name']?>')"><i class="fa-solid fa-comment"></i></button>
-            </div>
-            <div id="thread_comment<?=$list[$i]['wr_parent'];?>" class="thread_comment"></div>
-            <? } ?>
-		</div>
-    <? } elseif ($_list_style == 'list') { // 목록형
-        $_file_src = $list[$i]['file'][0]['file'] ? $list[$i]['file'][0]['path'].'/'.$list[$i]['file'][0]['file'] : '';
-        $_img_src = $list[$i]['wr_2'] ? $list[$i]['wr_2'] : $_file_src;
-        $_img_src = $list[$i]['wr_10'] ? $list[$i]['wr_10'] : $_img_src; // 썸네일 외부링크 우선
-    ?>
-        <div class="list-box">
-            <?php if ($is_checkbox) { ?>
-            <span class="td_chk">
-                <label for="chk_wr_id_<?php echo $i ?>" class="sound_only"><?php echo $list[$i]['subject'] ?></label>
-                <input type="checkbox" name="chk_wr_id[]" value="<?php echo $list[$i]['wr_id'] ?>" id="chk_wr_id_<?php echo $i ?>">
-            </span>
-            <?php } 
-            <a href="<?=$list[$i]['href']?>">
-            <div class="list_thumb<?=(!$_img_src) ? ' no_img' : ''; ?>">
-                <? if ($_img_src) { ?>
-                    <img src="<?=$_img_src; ?>" alt="">
-                <? } else { ?>
-                    <i class="fa-solid fa-list"></i>
-                <? } ?>
-            </div>
-            <? if ($list[$i]['wr_subject']) { ?><div class="list_title"><span><?=$list[$i]['wr_subject']; ?></span></div><? } ?>          
-            </a>
-        </div>
-    <? } ?>		
-		<? } ?>
-		<? if (count($list) == 0) { echo '<div class="no-data ">게시물이 없습니다.</div>'; } ?>
-	</div>
-	
-	<? if ($list_href || $is_checkbox || $write_href) { ?>
-	<div class="bo_fx txt-right">
-		<? if ($list_href || $write_href) { ?>
+                    <? } ?>
+                    <? if ($write_href) { ?>
+                    <div class="thread_bottom">
+                        <button type="button" onclick="writeThreadComment(<?=$list[$i]['wr_parent'];?>,'<?=$list[$i]['ca_name']?>')">댓글 달기</button>
+                    </div>
+                    <div id="thread_comment<?=$list[$i]['wr_parent'];?>" class="thread_comment" style="display:none;"></div>
+                    <? } ?>
+                </div>
+            <? } elseif ($_list_style == 'list') { 
+                $_file_src = $list[$i]['file'][0]['file'] ? $list[$i]['file'][0]['path'].'/'.$list[$i]['file'][0]['file'] : '';
+                $_img_src = $list[$i]['wr_2'] ? $list[$i]['wr_2'] : $_file_src;
+                $_img_src = $list[$i]['wr_10'] ? $list[$i]['wr_10'] : $_img_src; 
+            ?>
+                <div class="list-box">
+                    <?php if ($is_checkbox) { ?>
+                    <span class="td_chk">
+                        <label for="chk_wr_id_<?php echo $i ?>" class="sound_only"><?php echo $list[$i]['subject'] ?></label>
+                        <input type="checkbox" name="chk_wr_id[]" value="<?php echo $list[$i]['wr_id'] ?>" id="chk_wr_id_<?php echo $i ?>">
+                    </span>
+                    <?php } ?>
+                    <a href="<?=$list[$i]['href']?>">
+                    <div class="list_thumb<?=(!$_img_src) ? ' no_img' : ''; ?>">
+                        <? if ($_img_src) { ?>
+                            <img src="<?=$_img_src; ?>" alt="">
+                        <? } else { ?>
+                            <i class="fa-solid fa-list"></i>
+                        <? } ?>
+                    </div>
+                    <? if ($list[$i]['wr_subject']) { ?><div class="list_title"><span><?=$list[$i]['wr_subject']; ?></span></div><? } ?>          
+                    </a>
+                </div>
+            <? } ?>		
+        <? } ?>
+        <? if (count($list) == 0) { echo '<div class="no-data" style="text-align:center; padding:50px 0; background:#fff; border:1px solid #000;">게시물이 없습니다.</div>'; } ?>
+    </div>
+    
+    <div class="bo_fx txt-right">
+        <? if ($list_href || $write_href) { ?>
             <?php if ($is_checkbox && $_list_style == 'list') { ?> 
             <p class="chk_all">
                 <label for="chkall" class="sound_only">현재 페이지 게시물 전체</label>
                 <input type="checkbox" id="chkall" onclick="if (this.checked) all_checked(true); else all_checked(false);">
             </p> 
-		    <button type="submit" name="btn_submit" value="선택삭제" onclick="document.pressed=this.value" class="ui-btn admin">선택삭제</button>
+            <button type="submit" name="btn_submit" value="선택삭제" onclick="document.pressed=this.value" class="ui-btn admin">선택삭제</button>
             <?php } ?>
-		<? if ($list_href) { ?><a href="<? echo $list_href ?>" class="ui-btn">목록</a><? } ?>		
-		<? } ?>
-		<? if($admin_href){?><a href="<?=$admin_href?>" class="ui-btn admin" target="_blank">관리자</a><?}?>
-	</div>
-	<? } ?>
+            <? if ($list_href) { ?><a href="<? echo $list_href ?>" class="ui-btn">목록</a><? } ?>		
+        <? } ?>
+        <? if($admin_href){?><a href="<?=$admin_href?>" class="ui-btn admin" target="_blank">관리자</a><?}?>
+    </div>
 
     <? if ($_list_style == 'list') { ?>
     </form>
     <script>
     function all_checked(sw) {
         var f = document.fboardlist;
-
         for (var i=0; i<f.length; i++) {
             if (f.elements[i].name == "chk_wr_id[]")
                 f.elements[i].checked = sw;
         }
     }
-
     function fboardlist_submit(f) {
         var chk_count = 0;
-
         for (var i=0; i<f.length; i++) {
             if (f.elements[i].name == "chk_wr_id[]" && f.elements[i].checked)
                 chk_count++;
         }
-
         if (!chk_count) {
             alert(document.pressed + "할 게시물을 하나 이상 선택하세요.");
             return false;
@@ -162,7 +149,6 @@ if ($board['bo_use_list_file'] != '1') {
         if(document.pressed == "선택삭제") {
             if (!confirm("선택한 게시물을 정말 삭제하시겠습니까?\n\n한번 삭제한 자료는 복구할 수 없습니다\n\n답변글이 있는 게시글을 선택하신 경우\n답변글도 선택하셔야 게시글이 삭제됩니다."))
                 return false;
-
             f.removeAttribute("target");
             f.action = "./board_list_update.php";
         }
@@ -170,29 +156,9 @@ if ($board['bo_use_list_file'] != '1') {
     }
     </script>
     <? } ?>
-	<!-- 페이지 -->
-	<? echo $write_pages;  ?>
-
-	<!-- 게시판 검색 시작 { -->
-	<fieldset id="bo_sch" class="txt-center">
-		<legend>게시물 검색</legend>
-
-		<form name="fsearch" method="get">
-		<input type="hidden" name="bo_table" value="<? echo $bo_table ?>">
-		<input type="hidden" name="sca" value="<? echo $sca ?>">
-		<input type="hidden" name="sop" value="and">
-		<select name="sfl" id="sfl">
-			<option value="wr_subject"<? echo get_selected($sfl, 'wr_subject', true); ?>>제목</option>
-			<option value="wr_content"<? echo get_selected($sfl, 'wr_content'); ?>>내용</option>
-			<option value="wr_subject||wr_content"<? echo get_selected($sfl, 'wr_subject||wr_content'); ?>>제목+내용</option>
-		</select>
-		<input type="text" name="stx" value="<? echo stripslashes($stx) ?>" required id="stx" class="frm_input required" size="15" maxlength="20">
-		<button type="submit" class="ui-btn point ico search default"><i class="fas fa-search"></i></button>
-		</form>
-	</fieldset>
-	<!-- } 게시판 검색 끝 -->
+    
+    <? echo $write_pages;  ?>
 </div>
 </div>
-<? include_once($board_skin_path.'/thread.script.php'); // 타래 공통 스크립트 ?>
-<? include_once($board_skin_path.'/write.script.php'); // 글 작성시 이미지 관련 ?>
-<!-- } 게시판 목록 끝 -->
+<? include_once($board_skin_path.'/thread.script.php'); ?>
+<? include_once($board_skin_path.'/write.script.php'); ?>
--- thread_lt_comment/thread_lt_comment/view_comment.skin.php
+++ thread_lt_comment/thread_lt_comment/view_comment.skin.php
@@ -1,63 +1,84 @@
 <?
 if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 ?>
+<script>
+var char_min = parseInt(<? echo $comment_min ?>);
+var char_max = parseInt(<? echo $comment_max ?>);
+</script>
 
 <div class="board-comment-list board-comment-list<?=$list[$i]['wr_id'];?>">
-	<?
-	$cmt_amt = count($comment_list);
-	for ($c=0; $c<$cmt_amt; $c++) {
-		$comment_id = $comment_list[$c]['wr_id'];
-		$comment = $comment_list[$c]['content'];
-		$comment = markup_content($comment); 
+    <?
+    $cmt_amt = count($comment_list);
+    for ($c=0; $c<$cmt_amt; $c++) {
+        $comment_id = $comment_list[$c]['wr_id'];
+        $comment = $comment_list[$c]['content'];
+        $comment = preg_replace("/\[\<a\s.*href\=\"(http|https|ftp|mms)\:\/\/([^[:space:]]+)\.(mp3|wma|wmv|asf|asx|mpg|mpeg)\".*\<\/a\>\]/i", "<script>doc_write(obj_movie('$1://$2.$3'));</script>", $comment);
+        $comment = markup_content($comment); 
         $comment = textggu_change($comment); 
-		$comment = emote_ev($comment);
-	?>
-	<div class="cmt-item" id="c_<? echo $comment_id ?>">
-        <div class="cmt-profile">
-            <img src="https://i.pinimg.com/736x/ea/17/33/ea17336df0e8423cef735c0c708ef2fd.jpg" alt="프로필">
-        </div>
-        
-        <div class="cmt-content-wrap">
-            <div class="cmt-header">
-                <span class="cmt-name"><?=$comment_list[$c]['wr_name']; ?></span>
-                <span class="cmt-actions">
-                    <span style="color:#888; margin-right:5px;"><? echo date('m.d H:i', strtotime($comment_list[$c]['wr_datetime'])) ?></span>
-                    <? if($comment_list[$c]['is_edit']) { ?><a href="javascript:void(0)" onclick="comment_box('<? echo $comment_id ?>', 'ru','<?=$list[$i]['wr_id'];?>'); return false;">수정</a><? } ?>
-                    <? if($comment_list[$c]['is_del'])  { ?><a href="<? echo $comment_list[$c]['del_link'];  ?>" onclick="return comment_delete();">삭제</a><? } ?>
-                </span>
+        $comment = emote_ev($comment);
+    ?>
+    <div class="item" id="c_<? echo $comment_id ?>">
+        <div class="co_head_wrap">
+            <div class="co-name"><?=$comment_list[$c]['wr_name']; ?></div>
+            <div class="co-info">
+                <span><? echo date('m.d H:i', strtotime($comment_list[$c]['wr_datetime'])) ?></span>
+                <? if($comment_list[$c]['is_edit'] || $comment_list[$c]['is_del']) { ?>
+                    <? if ($comment_list[$c]['is_edit']) { ?><span><a href="javascript:void(0)" onclick="comment_box('<? echo $comment_id ?>', 'ru','<?=$list[$i]['wr_id'];?>'); return false;">수정</a></span><? } ?>
+                    <? if ($comment_list[$c]['is_del'])  { ?><span><a href="<? echo $comment_list[$c]['del_link']; ?>" onclick="return comment_delete();">삭제</a></span><? } ?>
+                <? } ?>
             </div>
-            <div class="cmt-text">
+        </div>
+        <div class="co-content">
+            <? if ($comment_list[$c]['wr_1'] == 'hide') { ?>
+                <button type="button" class="ui-btn" onclick="toggleComment('<?=$comment_id?>');">펼치기</button>
+            <? } ?>
+            <div class="co-inner co-inner<?=$comment_id?>"<?=($comment_list[$c]['wr_1'] == 'hide') ? ' style="display:none;"' : '';?>>
                 <? if (strstr($comment_list[$c]['wr_option'], "secret")) { ?><span class="secret" style="color:var(--kitsch-pink);">[비밀글]</span> <? } ?>
                 <? echo $comment ?>
             </div>
             
-            <span id="edit_<? echo $comment_id ?>"></span><span id="reply_<? echo $comment_id ?>"></span><textarea id="save_comment_<? echo $comment_id ?>_<?=$list[$i]['wr_id'];?>" style="display:none"><? echo get_text($comment_list[$c]['content1'], 0) ?></textarea>
+            <span id="edit_<? echo $comment_id ?>"></span>
+            <span id="reply_<? echo $comment_id ?>"></span>
+            <textarea id="save_comment_<? echo $comment_id ?>_<?=$list[$i]['wr_id'];?>" style="display:none"><? echo get_text($comment_list[$c]['content1'], 0) ?></textarea>
         </div>
-	</div>
-	<? } ?>
+    </div>
+    <? } ?>
 </div>
 
 <? if($c == 0) { ?>
-<script> $('.board-comment-list<?=$list[$i]['wr_id']?>').remove(); </script>
+<script>$('.board-comment-list<?=$list[$i]['wr_id']?>').remove();</script>
 <? } ?>
 
 <? if ($is_comment_write) { 
-	if($w == '') $w = 'r';
+    if($w == '') $w = 'r';
 ?>
 <div id="bo_vc_w_<?=$comment_id;?>" class="board-comment-write bo_vc_w_<?=$list[$i]['wr_id'];?>">
-	<form name="fviewcomment" action="<?=$board_skin_url; ?>/write_reply_update.php" onsubmit="return fviewreply_submit(this);" method="post" autocomplete="off">
-		<input type="hidden" name="w" value="<? echo $w ?>" id="w_<?=$list[$i]['wr_id'];?>">
-		<input type="hidden" name="bo_table" value="<? echo $bo_table ?>">
-		<input type="hidden" name="wr_id" value="<? echo $list[$i]['wr_id'] ?>">
-		<input type="hidden" name="comment_id" value="<? echo $c_id ?>" id="comment_id_<?=$list[$i]['wr_id'];?>">
-		
-		<div class="board-comment-form cmt-write">
-			<textarea id="wr_content_<?=$list[$i]['wr_id'];?>" name="wr_content" required class="required" placeholder="댓글을 남겨보세요..."><? echo $c_wr_content;  ?></textarea>
-			<div class="btn_confirm">
-				<button type="submit" id="btn_submit_<?=$list[$i]['wr_id'];?>" class="btn-cmt pixel-txt">등록</button>
-			</div>
-		</div>
-	</form>
+    <form name="fviewcomment" action="<?=$board_skin_url; ?>/write_reply_update.php" onsubmit="return fviewreply_submit(this);" method="post" autocomplete="off">
+        <input type="hidden" name="w" value="<? echo $w ?>" id="w_<?=$list[$i]['wr_id'];?>">
+        <input type="hidden" name="bo_table" value="<? echo $bo_table ?>">
+        <input type="hidden" name="wr_id" value="<? echo $list[$i]['wr_id'] ?>">
+        <input type="hidden" name="comment_id" value="<? echo $c_id ?>" id="comment_id_<?=$list[$i]['wr_id'];?>">
+        
+        <div class="board-comment-form">
+            <textarea id="wr_content_<?=$list[$i]['wr_id'];?>" name="wr_content" required class="required" placeholder="댓글을 남겨보세요..."><? echo $c_wr_content; ?></textarea>
+            <div class="btn_confirm">
+                <button type="submit" id="btn_submit_<?=$list[$i]['wr_id'];?>">등록</button>
+            </div>
+        </div>
+        
+        <?php if ($is_guest) { ?>
+        <p style="margin-top:5px;">
+            <input type="text" name="wr_name" value="<?php echo get_cookie("ck_sns_name"); ?>" required class="frm_input" size="10" placeholder="이름" style="border:1px solid #000; padding:3px;">
+            <input type="password" name="wr_password" required class="frm_input" size="10" placeholder="비밀번호" style="border:1px solid #000; padding:3px;">
+        </p>
+        <?php } ?>
+        
+        <p style="margin-top:5px; font-size:11px;">
+            <input type="checkbox" name="secret" value="secret" id="co_wr_secret_<?=$list[$i]['wr_id'];?>"> <label for="co_wr_secret_<?=$list[$i]['wr_id'];?>">비밀글</label> 
+            &nbsp;&nbsp;
+            <input type="checkbox" name="wr_1" value="hide" id="co_wr_1_<?=$list[$i]['wr_id'];?>"> <label for="co_wr_1_<?=$list[$i]['wr_id'];?>">접기</label>
+        </p>
+    </form>
 </div>
-<script> var save_html<?=$list[$i]['wr_id'];?> = document.getElementById('bo_vc_w_<?=$comment_id;?>').innerHTML; </script>
+<script>var save_html<?=$list[$i]['wr_id'];?> = document.getElementById('bo_vc_w_<?=$comment_id;?>').innerHTML;</script>
 <? } ?>
